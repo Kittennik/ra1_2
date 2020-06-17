addccd48f5b6cec6aae200ce11788c72dc2f34fd
AppVeyor <ci@appveyor.com>
2020-06-17 17:37:14 +0000

    c832a55

---
+++ precache-manifest.bb5b95bae966b525f125c0f4ce769e49.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ece1c113a41ed026b122506b09c3212e",
+    "url": "/ra1_2/index.html"
+  },
+  {
+    "revision": "e96b8f7b5c33a777ca87",
+    "url": "/ra1_2/static/css/main.e3548bff.chunk.css"
+  },
+  {
+    "revision": "438d6d99e1e85e5ffed9",
+    "url": "/ra1_2/static/js/2.b7bd71de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ra1_2/static/js/2.b7bd71de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e96b8f7b5c33a777ca87",
+    "url": "/ra1_2/static/js/main.22d9a812.chunk.js"
+  },
+  {
+    "revision": "d3e1a02d40ef7e4c7d1e",
+    "url": "/ra1_2/static/js/runtime-main.2c75ce3b.js"
+  },
+  {
+    "revision": "9c63a2c92146aa97904b9148bc055ce3",
+    "url": "/ra1_2/static/media/item-black.9c63a2c9.jpg"
+  },
+  {
+    "revision": "8bcbdffead0d5ba6700c1551189af1b8",
+    "url": "/ra1_2/static/media/item-blue.8bcbdffe.jpg"
+  }
+]);
